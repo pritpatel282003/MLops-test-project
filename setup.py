6cafb250c8ef5260166ef2acb38d73b83472d8dc
pritspatel03-png <prit.s.patel03@gmail.com>
2025-12-15 17:21:37 +0530

    venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="prit patel",
+    author_email="prit.s.patel03@gmail.com",
+    packages=find_packages()
+)
